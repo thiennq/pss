917eb1a6453e2b06d08910599c9b2b5435cde8eb
DESKTOP-LDPIQEB\PNQH <pnqh67@gmail.com>
2017-10-30 17:37:26 +0700

    api customer, api contact

--- models/Customer.php
+++ models/Customer.php
@@ -7,28 +7,37 @@ class Customer extends Illuminate\Database\Eloquent\Model {
     public $timestamps = false;
     protected $table = 'customer';
 
-    public function update($obj) {
-      $customer = Customer::where('phone', $obj->phone)->first();
-      if($customer) {
-        $customer->name = $obj->name;
-        $customer->email = $obj->email;
-        $customer->address = $obj->address;
-        $customer->region = $obj->region;
-        $customer->subregion = $obj->subregion;
+    public function store($data) {
+        $customer = new Customer;
+        $customer->name = $data['name'];
+        $customer->phone = $data['phone'];
+        $customer->email = $data['email'];
+        $customer->address = $data['address'];
+        $customer->region = $data['region'];
+        $customer->subregion = $data['subregion'];
+        $customer->created_at = date('Y-m-d H:i:s');
         $customer->updated_at = date('Y-m-d H:i:s');
-        $customer->save();
-        return $customer->id;
-      }
-      $customer = new Customer;
-      $customer->name = $obj->name;
-      $customer->phone = $obj->phone;
-      $customer->email = $obj->email;
-      $customer->address = $obj->address;
-      $customer->region = $obj->region;
-      $customer->subregion = $obj->subregion;
-      $customer->created_at = date('Y-m-d H:i:s');
-      $customer->updated_at = date('Y-m-d H:i:s');
-      $customer->save();
-      return $customer->id;
+        if($customer->save()) return $customer->id;
+    }
+
+    public function update($obj) {
+        $customer = Customer::where('phone', $obj->phone)->first();
+        if($customer) {
+            $customer->name = $obj->name;
+            $customer->email = $obj->email;
+            $customer->address = $obj->address;
+            $customer->region = $obj->region;
+            $customer->subregion = $obj->subregion;
+            $customer->updated_at = date('Y-m-d H:i:s');
+            $customer->save();
+            return $customer->id;
+        }
+    }
+
+    public function delete($id){
+        $customer = Customer::find($id);
+        if(!$customer) return -2;
+        if($customer->delete()) return $customer->id;
+        return -3;
     }
 }
